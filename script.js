278213e942d5541c33e3f2f2c5a1c2ee471f4cde
Anton <anton.zagarin@gmail.com>
2024-05-13 13:47:52 +0200

    create HW1

---
+++ script.js
@@ -0,0 +1,9 @@
+let num = 1;
+let str = 'String';
+let bool = true;
+let obj = {};
+let bigInt = 2349129391239128n;
+let nul = null;
+let undef;
+
+console.log(typeof num, typeof str, typeof bool, typeof bool, typeof obj, typeof bigInt, typeof nul, typeof undef);
